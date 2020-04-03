4b598a749819db63584386279df39faa9d352b42
horace-velmont <dragond7@naver.com>
2020-04-03 11:04:46 +0900

    BaseEntity 추가

--- src/main/java/nation/model/entity/Nation.java
+++ src/main/java/nation/model/entity/Nation.java
@@ -1,9 +1,11 @@
 package nation.model.entity;
 
+import core.model.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Nation {
+public class Nation extends BaseEntity {
+    private String name;
 }
